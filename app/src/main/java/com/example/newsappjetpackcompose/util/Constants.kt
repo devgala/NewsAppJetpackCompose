b6b30d0a9a704c6659ac8aa9e0a9e20906ee377b
priyank <priyank.shethia3@gmail.com>
2023-06-25 19:16:29 +0530

    Setup retorfit

---
+++ app/src/main/java/com/example/newsappjetpackcompose/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsappjetpackcompose.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "767531723d8542ea8bab5b6826c87485"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
