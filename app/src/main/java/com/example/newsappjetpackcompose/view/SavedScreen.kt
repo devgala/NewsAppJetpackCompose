afa1bbcfb01e5044489d2de9b9f556fa67d5345e
Dev Gala <114738592+devgala@users.noreply.github.com>
2023-06-26 01:11:16 +0530

    tp commit

--- app/src/main/java/com/example/newsappjetpackcompose/view/SavedScreen.kt
+++ app/src/main/java/com/example/newsappjetpackcompose/view/SavedScreen.kt
@@ -4,11 +4,33 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.newsappjetpackcompose.events.UiEventsSavedScreen
+import com.example.newsappjetpackcompose.viewmodel.SavedScreenViewModel
+
 
 @Composable
-fun SavedScreenUI() {
+fun SavedScreenUI(viewModel : SavedScreenViewModel) {
+    val articles = viewModel.articles.collectAsState(initial = emptyList())
+    LaunchedEffect(key1 = true){
+        viewModel.uiEvent.collect{
+            event ->
+            when(event){
+                is UiEventsSavedScreen.showSnackBar ->{
+
+                }
+                is UiEventsSavedScreen.openWebView ->{
+
+                }
+            }
+        }
+    }
+
+
+
     Box (modifier = Modifier.fillMaxSize()){
         Text(text = "SavedScreen", modifier = Modifier.align(Alignment.Center))
     }
